c5abe217484a4d3db031fffc647fd229b7a3b2e6
Dorani <seif.dorani@gmail.com>
2024-10-22 13:32:12 -0400

    edits

--- masterClass/3.Leetcode_Activities/are_dups.py
+++ masterClass/3.Leetcode_Activities/are_dups.py
@@ -9,10 +9,10 @@
 
 # Examples:
 
-areThereDuplicates(1, 2, 3) // false
-areThereDuplicates(1, 2, 2) // true
-areThereDuplicates('a', 'b', 'c', 'a') // true
-Restrictions:
+# areThereDuplicates(1, 2, 3) // false
+# areThereDuplicates(1, 2, 2) // true
+# areThereDuplicates('a', 'b', 'c', 'a') // true
+# Restrictions:
 
 # Time - O(n)
 # Space - O(n)
