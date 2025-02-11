98db935683b57c3560f4352db46289024c599630
Dorani <seif.dorani@gmail.com>
2025-02-11 12:25:21 -0500

    test1

--- masterClass/3.Leetcode_Activities/find_index_of_first_occurence.py
+++ masterClass/3.Leetcode_Activities/find_index_of_first_occurence.py
@@ -14,3 +14,7 @@ def strStr(haystack: str, needle: str) -> int:
     
     # If no match found, return -1
     return -1
+
+haystack = "sadbutsad"
+needle = "sad"
+print(strStr(haystack, needle))  # Output: 0
